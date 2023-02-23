329c9b4f3c9a10517d23128fcfe93cd85860477a
alvin <alvin@DESKTOP-AE8PM2K>
2023-02-23 17:35:03 +0530

    create  project

---
+++ Magic villa/Controllers/VillaAPIController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Magic_villa.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        private readonly ILogger<VillaAPIController> _logger;
+        public VillaAPIController(ILogger<VillaAPIController> logger)
+        {
+                _logger = logger;
+        }
+        []
+    }
+}
